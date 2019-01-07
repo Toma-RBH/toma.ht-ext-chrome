87d41325c617d4981975d86d3498e857440243a3
TOMA <46223123+Toma-RBH@users.noreply.github.com>
2019-01-07 12:39:45 -0500

    Add files via upload

---
+++ background.js
@@ -0,0 +1,21 @@
+
+// create a context menu
+chrome.contextMenus.create({
+    id:"analyseWithToma",
+    title:"Analysez avec Toma",
+    contexts:["all"]
+});
+
+
+// add action listener
+chrome.contextMenus.onClicked.addListener(do_action);
+
+function do_action(info, tab){
+    const url = "https://toma.ht/#/?url="+tab.url;
+    
+    chrome.tabs.create({
+        url:url
+    });
+
+}
+
